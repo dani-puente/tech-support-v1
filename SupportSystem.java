b12f0ee95daebd62ece1cb0b0735b83abebbab8c
dani-puente <dpuentet@hotmail.com>
2021-02-18 10:01:31 +0100

    Sale del metodo si bye contiene mayus o minusculas

--- SupportSystem.java
+++ SupportSystem.java
@@ -38,7 +38,7 @@ public class SupportSystem
 
         while(!finished) {
             String input = reader.getInput();
-            
+            input = input.toLowerCase();
             if(input.contains("bye")) {
                 finished = true;
             }
