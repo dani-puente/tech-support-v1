4f54b23f0a92f54f3b110225f50bd2b5f2c02331
dani-puente <dpuentet@hotmail.com>
2021-03-10 09:23:40 +0100

    Creamos una biblioteca de respuestas asociadas a unas determinadas palabras

--- Responder.java
+++ Responder.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.HashMap;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -9,25 +10,38 @@ import java.util.ArrayList;
  */
 public class Responder{
     private Random ran;
-    private ArrayList<String> array;
+    private ArrayList<String> respuestas;
+    private HashMap<String, String> respuestasAsociadas;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder(){
         ran = new Random();
-        array = new ArrayList<>();
-        array.add("Eso es facil de resolver");
-        array.add("Eso va a dar problemas...");
-        array.add("Necesito verlo en persona");
-        array.add("Ejecuta ctrl+alt F5");
-        array.add("Habra que cambiar el equipo");
+        respuestas = new ArrayList<>();
+        respuestas.add("Eso es facil de resolver");
+        respuestas.add("Eso va a dar problemas...");
+        respuestas.add("Necesito verlo en persona");
+        respuestas.add("Ejecuta ctrl+alt F5");
+        respuestas.add("Habra que cambiar el equipo");
+        respuestasAsociadas = new HashMap<>();
+        respuestasAsociadas.put("raton", "Conectalo y desconectalo");
+        respuestasAsociadas.put("teclado", "Al ser bluethoot prueba a emparejarlo");
+        respuestasAsociadas.put("monitor", "Comprueba el cable de corriente");
+        respuestasAsociadas.put("disco duro", "Habra que pasarle un software para ver su estado");
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse(){
-        return array.get(ran.nextInt(array.size()));
+    public String generateResponse(String userInput){
+        String aDevolver = "";
+        String textoABuscar = userInput;
+        if(respuestasAsociadas.get(textoABuscar) != null){
+           aDevolver = respuestasAsociadas.get(textoABuscar);
+        }else{
+            aDevolver = respuestas.get(ran.nextInt(respuestas.size()));
+        }
+        return aDevolver;
     }
 }
